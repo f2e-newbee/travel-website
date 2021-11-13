4673f2bcbcbe1a904108d4bbb0648d3fcc46daa6
Joanne Lin <joanne@miniasp.com>
2021-11-13 14:42:37 +0800

    refractor: 調整列表頁邏輯

--- src/features/attractionlist/AttractionItem.js
+++ src/features/attractionlist/AttractionItem.js
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 import Slider from "react-slick";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
+import CustomHeader from "../../components/customHeader/CustomHeader";
 
 const sliderSettings = {
   dots: false,
@@ -75,67 +76,70 @@ const AttractionItem = () => {
   // };
 
   return (
-    <div className="container mx-auto">
-      {/* 區塊一 */}
-      <div className="grid grid-cols-2 gap-10 py-10 ">
-        {data && (
-          <img
-            src={data.Picture.PictureUrl1}
-            alt={data.Picture.PictureDescription1}
-          />
-        )}
-
-        <p className="text-primary-dark flex items-center leading-8  text-center font-bold">
-          {data && data.DescriptionDetail}
-        </p>
-      </div>
-      {/* 景點資訊  */}
-      <div className="grid grid-cols-2 gap-10 py-10">
-        <Title className="col-span-2">景點資訊</Title>
-        {data && (
-          <div>
-            <h4 className="text-primary-dark">地址：{data.Address}</h4>
-            <h4 className="text-primary-dark">電話：{data.Phone}</h4>
-            <h4 className="text-primary-dark">營業時間：{data.OpenTime}</h4>
-            <h4 className="text-primary-dark">分類：{data.Class1}</h4>
-            <div className="text-secondary-dark">
-              店家資訊僅供參考，詳細資訊請洽業者
-            </div>
-          </div>
-        )}
-        <div>
+    <>
+      {/* <CustomHeader title={data.Name}></CustomHeader> */}
+      <div className="container mx-auto">
+        {/* 區塊一 */}
+        <div className="grid grid-cols-2 gap-10 py-10 ">
           {data && (
             <img
               src={data.Picture.PictureUrl1}
               alt={data.Picture.PictureDescription1}
             />
           )}
+
+          <p className="text-primary-dark flex items-center leading-8  text-center font-bold">
+            {data && data.DescriptionDetail}
+          </p>
         </div>
-      </div>
-
-      {/* 交通資訊 */}
-      <Title>交通資訊</Title>
-      <div className="grid grid-cols-2 gap-10 py-10"></div>
-
-      {/* 周邊美食  */}
-      <Title>周邊美食</Title>
-      <Slider {...sliderSettings}>
-        {restaurant &&
-          restaurant.map((item) => {
-            return (
-              <div>
-                <ImageListItem>
-                  <img
-                    src={item.Picture.PictureUrl1}
-                    alt={item.Picture.PictureDescription1}
-                  />
-                  <ImageListItemBar title={item.Name} />
-                </ImageListItem>
+        {/* 景點資訊  */}
+        <div className="grid grid-cols-2 gap-10 py-10">
+          <Title className="col-span-2">景點資訊</Title>
+          {data && (
+            <div>
+              <h4 className="text-primary-dark">地址：{data.Address}</h4>
+              <h4 className="text-primary-dark">電話：{data.Phone}</h4>
+              <h4 className="text-primary-dark">營業時間：{data.OpenTime}</h4>
+              <h4 className="text-primary-dark">分類：{data.Class1}</h4>
+              <div className="text-secondary-dark">
+                店家資訊僅供參考，詳細資訊請洽業者
               </div>
-            );
-          })}
-      </Slider>
-    </div>
+            </div>
+          )}
+          <div>
+            {data && (
+              <img
+                src={data.Picture.PictureUrl1}
+                alt={data.Picture.PictureDescription1}
+              />
+            )}
+          </div>
+        </div>
+
+        {/* 交通資訊 */}
+        <Title>交通資訊</Title>
+        <div className="grid grid-cols-2 gap-10 py-10"></div>
+
+        {/* 周邊美食  */}
+        <Title>周邊美食</Title>
+        <Slider {...sliderSettings}>
+          {restaurant &&
+            restaurant.map((item) => {
+              return (
+                <div>
+                  <ImageListItem>
+                    <img
+                      src={item.Picture.PictureUrl1}
+                      alt={item.Picture.PictureDescription1}
+                    />
+                    <ImageListItemBar title={item.Name} />
+                  </ImageListItem>
+                </div>
+              );
+            })}
+        </Slider>
+      </div>
+    </>
   );
 };
 
