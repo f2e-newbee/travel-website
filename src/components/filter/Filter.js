2e7482dbcc3a45e0dd3b78c77ae858983980889e
Joanne Lin <joanne@miniasp.com>
2021-11-14 16:54:06 +0800

    feat: 景點列表關鍵字搜尋 & 縣市Filter基礎功能 & 景點列表手機板設一Col

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,32 +1,118 @@
-import React, { useState } from "react";
-import SortIcon from "@mui/icons-material/Sort";
-import MenuItem from "@mui/material/MenuItem";
+import React from "react";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 
-const Filter = () => {
-  const [district, setDistrict] = useState("");
+const cityList = [
+  {
+    text: "臺北市",
+    value: "Taipei",
+  },
+  {
+    text: "新北市",
+    value: "NewTaipei",
+  },
+  {
+    text: "桃園市",
+    value: "Taoyuan",
+  },
+  {
+    text: "臺中市",
+    value: "Taichung",
+  },
+  {
+    text: "臺南市",
+    value: "Tainan",
+  },
+  {
+    text: "高雄市",
+    value: "Kaohsiung",
+  },
+  {
+    text: "基隆市",
+    value: "Keelung",
+  },
+  {
+    text: "新竹市",
+    value: "Hsinchu",
+  },
+  {
+    text: "新竹縣",
+    value: "HsinchuCounty",
+  },
+  {
+    text: "苗栗縣",
+    value: "MiaoliCounty",
+  },
+  {
+    text: "彰化縣",
+    value: "ChanghuaCounty",
+  },
+  {
+    text: "南投縣",
+    value: "NantouCounty",
+  },
+  {
+    text: "雲林縣",
+    value: "YunlinCounty",
+  },
+  {
+    text: "嘉義縣",
+    value: "ChiayiCounty",
+  },
+  {
+    text: "嘉義市",
+    value: "Chiayi",
+  },
+  {
+    text: "屏東縣",
+    value: "PingtungCounty",
+  },
+  {
+    text: "宜蘭縣",
+    value: "YilanCounty",
+  },
+  {
+    text: "花蓮縣",
+    value: "HualienCounty",
+  },
+  {
+    text: "臺東縣",
+    value: "TaitungCounty",
+  },
+  {
+    text: "金門縣",
+    value: "KinmenCounty",
+  },
+  {
+    text: "澎湖縣",
+    value: "PenghuCounty",
+  },
+  {
+    text: "連江縣",
+    value: "LienchiangCounty",
+  },
+];
 
-  const handleDistrictChange = (event) => {
-    setDistrict(event.target.value);
-  };
-
+const Filter = ({ city, handleCityChange }) => {
   return (
-    <Stack direction="row" alignItems="center" spacing={2}>
-      <div>
-        <SortIcon />
-        <span>排序</span>
-      </div>
-      <Chip label="評分" variant="outlined" />
-      <Chip label="評論數" variant="outlined" />
-      <FormControl variant="standard">
-        <Select value={district} onChange={handleDistrictChange}>
-          <MenuItem value={10}>Ten</MenuItem>
-        </Select>
-      </FormControl>
-    </Stack>
+    <FormControl className="w-40" variant="standard">
+      <InputLabel>縣市</InputLabel>
+      <Select
+        value={city}
+        label="City"
+        onChange={(event) => handleCityChange(event.target.value)}
+      >
+        {cityList.map((city) => {
+          return (
+            <MenuItem key={city.value} value={city.value}>
+              {city.text}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </FormControl>
   );
 };
 
