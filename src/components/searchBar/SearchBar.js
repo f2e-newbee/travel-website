e5082cbe30a18c6b329176f2ce52371f505c10d6
Joanne Lin <joanne@miniasp.com>
2021-11-14 22:10:52 +0800

    feat:景點列表篩選邏輯

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-
+import Filter from "../filter/Filter";
 /**
  * 關鍵字搜尋元件
  */
 
-const SearchBar = ({ keyWord, setKeyWord, handleSearch, type }) => {
+const SearchBar = ({
+  keyWord,
+  setKeyWord,
+  handleSearch,
+  type,
+  city,
+  setCity,
+}) => {
   return (
-    <div className="flex w-full justify-center">
+    <div className="w-full flex items-center justify-center">
+      <div className="w-40">
+        <Filter city={city} setCity={setCity} />
+      </div>
       <div className="bg-gray-300 w-full max-w-xs	py-2 px-4 rounded-3xl flex items-center text-white">
         <SearchIcon className="inline-block mr-2" />
         <input
@@ -18,19 +28,21 @@ const SearchBar = ({ keyWord, setKeyWord, handleSearch, type }) => {
           onChange={(e) => setKeyWord(e.target.value)}
         />
       </div>
-      {type === "primary" && (
-        <button
-          className="bg-primary hover:bg-primary-dark py-2 px-6 rounded-3xl text-white font-bold ml-4"
-          onClick={() => handleSearch(keyWord)}
-        >
-          開始搜尋
-        </button>
-      )}
-      {type === "secondary" && (
-        <button className="bg-secondary hover:bg-secondary-dark py-2 px-6 rounded-3xl text-white font-bold ml-4">
-          開始搜尋
-        </button>
-      )}
+      <div>
+        {type === "primary" && (
+          <button
+            className="bg-primary hover:bg-primary-dark py-2 px-6 rounded-3xl text-white font-bold ml-4"
+            onClick={() => handleSearch(keyWord)}
+          >
+            開始搜尋
+          </button>
+        )}
+        {type === "secondary" && (
+          <button className="bg-secondary hover:bg-secondary-dark py-2 px-6 rounded-3xl text-white font-bold ml-4">
+            開始搜尋
+          </button>
+        )}
+      </div>
     </div>
   );
 };
